4518d76566c77fb4eaa32beb817d12a6b136c081
Scoican Ovidiu <scoican.ovidiu@gmail.com>
2019-11-22 10:36:54 +0200

    Map generation with Perlin Noise

--- Cronosferum/Assets/Scripts/HexCell.cs
+++ Cronosferum/Assets/Scripts/HexCell.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HexCell : MonoBehaviour {
+public class HexCell : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private float height;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public float getHeight()
+    {
+
+        return this.transform.localScale.y;
+    }
+
+    public void setHeight(float newHeight)
+    {
+        this.transform.localScale = new Vector3(this.transform.localScale.x, newHeight, this.transform.localScale.z);
+        this.transform.position = new Vector3(this.transform.position.x, 0, this.transform.position.z);
+    }
 }
--- Cronosferum/Assets/Scripts/HexGrid.cs
+++ Cronosferum/Assets/Scripts/HexGrid.cs
@@ -4,41 +4,127 @@ using UnityEngine;
 
 public class HexGrid : MonoBehaviour
 {
+    public GameObject hexPrefab;
 
-    public int width = 6;
-    public int height = 6;
+    //Size of the map in terms of the number of hex tiles
+    public int width = 20;
+    public int height = 20;
 
-    public HexCell cellPrefab;
+    //Distance between 2 rows of tiles
+    //This will be half on odd rows beaceasue of the hex form
+    float xOffset = 0.882f;
 
-    HexCell[] cells;
+    //Distance between 2 hex tiles
+    //If this is missing, tilles will overlap
+    float zOffset = 0.764f;
 
-    //Generate the hex grid when this objects is awaken
-    //What is awake?
-    //TO DO: make the grid hexagonal
-    void Awake()
+    //Perlin values
+    public float perlinScale = 20f;
+    float seed;
+
+    // Use this for initialization
+    void Start()
     {
-        cells = new HexCell[height * width];
+        this.GenerateGrid();
+    }
 
-        for (int z = 0, i = 0; z < height; z++)
+    void GenerateGrid()
+    {
+        seed = Random.Range(0f, 99999f);
+        for (int x = 0; x < width; x++)
         {
-            for (int x = 0; x < width; x++)
+            for (int z = 0; z < height; z++)
             {
-                CreateCell(x, z, i++);
+                float xPos = x * xOffset;
+
+                //Are we on an odd row?
+                if (z % 2 == 1)
+                {
+                    xPos += xOffset / 2;
+                }
+
+                var newHex = Instantiate(hexPrefab, new Vector3(xPos, 0, z * zOffset), Quaternion.identity);
+                this.GenerateTerrain(newHex, x, z);
             }
         }
     }
 
-    //Method that creates one cell in the grid
-    void CreateCell(int x, int z, int i)
+    void GenerateTerrain(GameObject hex, int xPos, int zPos)
     {
-        Vector3 position;
-        position.x = x * 10f;
-        position.y = 0f;
-        position.z = z * 10f;
-
-        HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
-        cell.transform.SetParent(transform, false);//what is false?
-        cell.transform.localPosition = position;
+        int perlinValue = this.CalculatePerlinValue(xPos, zPos);
+        Color color = CalculateColor(xPos, zPos);
+        var hexModel = hex.GetComponent<HexCell>();
+        var hexRenderer = hex.GetComponentInChildren<Renderer>();
+        hexModel.setHeight(perlinValue);
+        hexRenderer.material.SetColor("_Color", color);
     }
 
+
+
+    int CalculatePerlinValue(int x, int z)
+    {
+        float xPerlinCoord = (float)x / 256 * perlinScale + seed;
+        float zPerlinCoord = (float)z / 256 * perlinScale + seed;
+
+        float sample = Mathf.PerlinNoise(xPerlinCoord, zPerlinCoord);
+        if (sample < 0.35f)
+        {
+            return 1;
+        }
+        else if (sample >= 0.35f && sample < 0.60f)
+        {
+            return 3;
+        }
+        else if (sample >= 0.60f && sample <= 1)
+        {
+            return 5;
+        }
+        return 1;
+    }
+
+    Color CalculateColor(int x, int z)
+    {
+        float xPerlinCoord = (float)x / 256 * perlinScale + seed;
+        float zPerlinCoord = (float)z / 256 * perlinScale + seed;
+
+        float sample = Mathf.PerlinNoise(xPerlinCoord, zPerlinCoord);
+        if (sample < 0.35f)
+        {
+            sample += 0.1f;
+            return new Color32(getPerlinColor(102, sample), getPerlinColor(153, sample), getPerlinColor(255, sample), 20);
+        }
+        else if (sample >= 0.35f && sample < 0.60f)
+        {
+            return new Color32(getPerlinColor(0, sample), getPerlinColor(204, sample), getPerlinColor(0, sample), 255);
+        }
+        else if (sample >= 0.60f && sample <= 1)
+        {
+            return new Color32(getPerlinColor(204, sample), getPerlinColor(102, sample), getPerlinColor(0, sample), 255);
+        }
+        return new Color32(102, 153, 255, 20);
+    }
+
+
+    byte getPerlinColor(byte colorValue, float perlinSample)
+    {
+        return (byte)((perlinSample * colorValue) + (perlinSample * colorValue) * 0.2f);
+    }
+
+    /*
+	Color CalculateColor(int perlinValue)
+    {
+        if (perlinValue == 1)
+        {
+            return WATER;
+        }
+        if (perlinValue == 3)
+        {
+            return FOREST;
+        }
+        if (perlinValue == 5)
+        {
+            return MOUNTAIN;
+        }
+        return FOREST;
+    }*/
 }
